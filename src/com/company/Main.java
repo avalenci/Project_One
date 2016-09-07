0310e4863354e8e8b32e554fbca0603353d6e75c
Andrew B Valenci <av128@pvhs-lab112-d05.pvsd.org>
2016-09-07 14:20:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+        System.out.print("Dlrow Olleh"); //Great way to document your code.
+        /* Two types of print, ln uses return or enter after it prints.
+        There are also two ways to make comments.
+         */
+        System.out.print(1);
+        System.out.print(1.0);
+    }
+}
